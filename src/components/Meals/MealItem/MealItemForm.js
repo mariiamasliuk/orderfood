c34a817ea5efe50724a159658f1db162186bded2
Mariia Masliuk <mariia_masliuk@epam.com>
2024-05-28 21:10:22 +0300

    MealItem component was added

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./MealItemForm.module.scss";
-
+import Input from "../../UI/Input";
 const MealItemForm = (props) => {
   return (
     <form className={styles.form}>
-      <input />
+      <Input
+        label='Amount'
+        input={{
+          id: "amount",
+          type: "number",
+          min: 1,
+          max: 5,
+          step: 1,
+          defaultValue: 1,
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
---
+++ src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Input.module.scss";
+
+const Input = (props) => {
+  return (
+    <div className={styles.input}>
+      {props.label && <label htmlFor={props.input.id}>{props.label}</label>}
+      <input id={props.input.id} {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
